e38defd5e542d530f187f0c24730e0846539f54b
iwe7 <admin@meepo.com.cn>
2018-05-12 15:34:49 +0800

    更新

--- projects/elements/src/lib/global.ts
+++ projects/elements/src/lib/global.ts
@@ -3,7 +3,5 @@ declare const window: any;
 (<any>window).ElementsLoad = (<any>window).ElementsLoad || of(null);
 (<any>window).ElementsMap = (<any>window).ElementsMap || new Map();
 
-declare let ElementsLoad: Observable<any>;
-declare let ElementsMap: Map<string, any>;
-
-export { ElementsLoad, ElementsMap };
+export let ElementsLoad: Observable<any> = (<any>window).ElementsLoad;
+export let ElementsMap: Map<string, any> = (<any>window).ElementsMap;
